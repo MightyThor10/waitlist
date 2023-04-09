77ef9247b1d292911d723a09424a36ae66487019
azheng04 <azheng04@wm.edu>
2023-04-09 11:05:42 -0400

    logout

--- Code/studentview/urls.py
+++ Code/studentview/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from. import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='student-home'),
     path('joinwaitlist/',views.joinWaitlist, name='join-waitlist'),
+
 ]
