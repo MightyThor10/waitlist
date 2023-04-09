4053d6ffc4c119f96e56dd8c2447293ca91dad70
seanlindell <lindellsean@gmail.com>
2023-04-09 13:28:53 -0400

    signing up from the student view adds user to Student group

--- Code/users/views.py
+++ Code/users/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.models import Group
 # Create your views here.
 def register(request):
     if request.method =='POST':
         form =UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save() #automatically hashes password and saves user
+            user = form.save() #automatically hashes password and saves user
+            group = Group.objects.get(name='Student')
+            user.groups.add(group)
+            user.save()
             first = form.cleaned_data.get('first_name')
             last = form.cleaned_data.get('last_name')
             messages.success(request,f'Account created for {first} {last}')
