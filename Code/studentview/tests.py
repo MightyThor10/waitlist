ef4f7235b141e760b3bba77bd089ac8d881ee671
srazmjo <srazmjo@wm.edu>
2023-04-10 10:57:19 -0400

    added test cases for users and studentview. CI/CD ready. added gitlab.yml file

--- Code/studentview/tests.py
+++ Code/studentview/tests.py
@@ -1,3 +1,82 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User, Group
+from .models import ClassWaitlist, StudentTicket
+from .views import home, joinWaitlist
 
-# Create your tests here.
+class ClassWaitlistModelTests(TestCase):
+
+    def test_create_class_waitlist(self):
+        user = User.objects.create_user(username='test_professor', password='testpassword')
+        class_waitlist = ClassWaitlist.objects.create(
+            className="Test Class",
+            classCode="TEST101",
+            crn=12345,
+            schedule="MWF 10:00-11:00",
+            sortType="FIFO",
+            term="Fall 2023",
+            professor=user,
+            date_added="2023-04-10"
+        )
+        self.assertEqual(class_waitlist.className, "Test Class")
+
+class StudentTicketModelTests(TestCase):
+
+    def test_create_student_ticket(self):
+        student = User.objects.create_user(username='test_student', password='testpassword')
+        professor = User.objects.create_user(username='test_professor', password='testpassword')
+        class_waitlist = ClassWaitlist.objects.create(
+            className="Test Class",
+            classCode="TEST101",
+            crn=12345,
+            schedule="MWF 10:00-11:00",
+            sortType="FIFO",
+            term="Fall 2023",
+            professor=professor,
+            date_added="2023-04-10"
+        )
+        student_ticket = StudentTicket.objects.create(
+            class_waitlist=class_waitlist,
+            date_joined="2023-04-10",
+            student=student
+        )
+        self.assertEqual(student_ticket.student.username, "test_student")
+
+class HomeViewTests(TestCase):
+
+    def test_home_view_no_login(self):
+        response = self.client.get(reverse('student-home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Log in to view your classes!")
+
+    def test_home_view_legacy_account(self):
+        user = User.objects.create_user(username='legacy_user', password='testpassword')
+        self.client.login(username='legacy_user', password='testpassword')
+        response = self.client.get(reverse('student-home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You are not logged in as a professor or a student!")
+
+class JoinWaitlistViewTests(TestCase):
+
+    def test_join_waitlist_view(self):
+        response = self.client.get(reverse('join-waitlist'))
+        self.assertEqual(response.status_code, 200)
+        #self.assertContains(response, "waitlist")
+
+class DetailViewTests(TestCase):
+
+    def test_detail_view(self):
+        user = User.objects.create_user(username='test_professor', password='testpassword')
+        class_waitlist = ClassWaitlist.objects.create(
+            className="Test Class",
+            classCode="TEST101",
+            crn=12345,
+            schedule="MWF 10:00-11:00",
+            sortType="FIFO",
+            term="Fall 2023",
+            professor=user,
+            date_added="2023-04-10"
+        )
+        response = self.client.get(reverse('detail', args=(class_waitlist.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Class")
--- Code/users/tests.py
+++ Code/users/tests.py
@@ -1,3 +1,74 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User, Group
+from users.forms import UserRegisterForm
 
-# Create your tests here.
+class UserRegisterFormTests(TestCase):
+
+    def test_valid_form(self):
+        form = UserRegisterForm({
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'johndoe@email.wm.edu',
+            'password1': 'strong_password123',
+            'password2': 'strong_password123'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_email_domain(self):
+        form = UserRegisterForm({
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'johndoe@example.com',
+            'password1': 'strong_password123',
+            'password2': 'strong_password123'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors)
+
+class RegisterViewTests(TestCase):
+
+    def setUp(self):
+        self.student_group = Group.objects.create(name='Student')
+
+    def test_register_view_uses_correct_template(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_register_view_creates_user_and_adds_to_group(self):
+        response = self.client.post(reverse('register'), {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'johndoe@email.wm.edu',
+            'password1': 'strong_password123',
+            'password2': 'strong_password123'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
+
+        user = User.objects.get(username='johndoe')
+        self.assertIsNotNone(user)
+
+        group = Group.objects.get(name='Student')
+        self.assertTrue(user.groups.filter(name='Student').exists())
+
+
+class ProfileViewTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='johndoe',
+            password='strong_password123',
+            email='johndoe@email.wm.edu'
+        )
+
+
+    def test_profile_view_uses_correct_template(self):
+        self.client.login(username='johndoe', password='strong_password123')
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/profile.html')
