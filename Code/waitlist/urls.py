0551cd9f521ecda53c9a7bd3203be7336a4eda87
Isaac Isaac Mast <gimast@email.wm.edu>
2023-04-09 21:34:25 -0400

    Refactor django project, app, and url structure

--- Code/waitlist/urls.py
+++ Code/waitlist/urls.py
@@ -1,6 +1,25 @@
-from django.urls import path
-from waitlist.views import waitlistHome
+"""WaitlistWM URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
+import waitlist.users.views as user_views
 
 urlpatterns = [
-    path("", waitlistHome, name="Waitlist"),
+    path('admin/', admin.site.urls),
+    path('studenthome/', include('waitlist.studentview.urls')),
+    path('user/', include('waitlist.users.urls')),
 ]
--- Code/waitlist/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def waitlistHome(request):
-    return HttpResponse("Hello, World!")
