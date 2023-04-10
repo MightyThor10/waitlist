47922e68fa1ff3117bf62da442fb1ae1be4c09ea
seanlindell <lindellsean@gmail.com>
2023-04-09 21:31:26 -0400

    students can see their active waitlists and click on a button to view details

--- Code/studentview/views.py
+++ Code/studentview/views.py
@@ -11,34 +11,26 @@ def home(request):
     currentUser = request.user
     groupOfUser = currentUser.groups.all().first()
 
-    classes = [
-            {
-                'className': 'CSCI464-01',
-                'professor': 'John DOe',
-                'schedule':'MWF 11:00AM - 11:50AM',
-                'crn':'XXXXX',
-                'waitlistposition':'3/25 in line'
-            },
-            {
-                'className': 'CSCI101-01',
-                'professor': 'Janet DOe',
-                'schedule':'MWF 10:00AM - 11:50AM',
-                'crn':'XXXX3',
-                'waitlistposition':'4/25 in line'
-            }
-        ]   
+    classes = []
+    message =  ""
 
     if (not currentUser.is_anonymous):
         if (groupOfUser.id == 1):
             classes = ClassWaitlist.objects.filter(professor=currentUser.pk)
-    # if (groupOfUser.id == 2):
-    #     studentTickets = StudentTicket(student=currentUser.pk)
-    #     print(studentTickets)
-    #     #classes = ClassWaitlist(professor=currentUser.pk)
+        if (groupOfUser.id == 2):
+            studentTickets = StudentTicket.objects.filter(student=currentUser.pk)
+            classPKs = set()
+            for ticket in studentTickets:
+                classPKs.add(ticket.class_waitlist.pk)
+            classPKs = list(classPKs)
+            classes = ClassWaitlist.objects.filter(pk__in=classPKs)
+    if (currentUser.is_anonymous):
+        message = "Log in to view your classes!"
          
 
     context={
-        'classes':classes
+        'classes':classes,
+        'message':message
     }
     return render(request,'studentview/home.html', context)
 
