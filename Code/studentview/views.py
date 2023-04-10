c5432d2fc9a6b45fb53ab5c0bb9000503014dbc1
seanlindell <lindellsean@gmail.com>
2023-04-09 21:15:28 -0400

    professors can see their active waitlists and click on a button to view details

--- Code/studentview/views.py
+++ Code/studentview/views.py
@@ -1,27 +1,42 @@
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
-from .models import ClassWaitlist
+from .models import ClassWaitlist, StudentTicket
 from django.contrib.auth.models import User, Group
 
 # Create your views here.
-classes = [
-    {
-        'classname': 'CSCI464-01',
-        'professor': 'John DOe',
-        'datetime':'MWF 11:00AM - 11:50AM',
-        'CRN':'XXXXX',
-        'waitlistposition':'3/25 in line'
-    },
-    {
-        'classname': 'CSCI101-01',
-        'professor': 'Janet DOe',
-        'datetime':'MWF 10:00AM - 11:50AM',
-        'CRN':'XXXX3',
-        'waitlistposition':'4/25 in line'
-    }
-]
+
 
 def home(request):
+
+    currentUser = request.user
+    groupOfUser = currentUser.groups.all().first()
+
+    classes = [
+            {
+                'className': 'CSCI464-01',
+                'professor': 'John DOe',
+                'schedule':'MWF 11:00AM - 11:50AM',
+                'crn':'XXXXX',
+                'waitlistposition':'3/25 in line'
+            },
+            {
+                'className': 'CSCI101-01',
+                'professor': 'Janet DOe',
+                'schedule':'MWF 10:00AM - 11:50AM',
+                'crn':'XXXX3',
+                'waitlistposition':'4/25 in line'
+            }
+        ]   
+
+    if (not currentUser.is_anonymous):
+        if (groupOfUser.id == 1):
+            classes = ClassWaitlist.objects.filter(professor=currentUser.pk)
+    # if (groupOfUser.id == 2):
+    #     studentTickets = StudentTicket(student=currentUser.pk)
+    #     print(studentTickets)
+    #     #classes = ClassWaitlist(professor=currentUser.pk)
+         
+
     context={
         'classes':classes
     }
