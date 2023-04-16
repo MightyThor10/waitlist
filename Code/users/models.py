a9d90289f7a774339ab63e836b46ce756c25a6c4
srazmjo <srazmjo@wm.edu>
2023-04-16 18:18:06 -0400

    implemented change password functionality, implemented profile page with updatable fields, such as academic stastus, major and preferred name

--- Code/users/models.py
+++ Code/users/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    preferred_name = models.CharField(max_length=100, blank=True)
+    academic_status = models.CharField(max_length=20, blank=True)
+    major = models.CharField(max_length=100, blank=True)
+@receiver(post_save, sender=User)
+def create_student_profile(sender, instance, created, **kwargs):
+    if created:
+        StudentProfile.objects.create(user=instance)
 
-
+@receiver(post_save, sender=User)
+def save_student_profile(sender, instance, **kwargs):
+    instance.studentprofile.save()
