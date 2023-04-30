eb31a7e615c541c319d14efe08979bacce5300a4
awallan <awallan@email.wm.edu>
2023-04-29 23:50:54 -0400

    added Major and Academic Status to join request

--- Code/studentview/models.py
+++ Code/studentview/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class ClassWaitlist(models.Model):
     className = models.CharField(max_length=200, name="className", verbose_name='Class Name')
     classDescription = models.TextField(verbose_name='Description', default=' ')
-    classCode = models.CharField(max_length=200, verbose_name='Class Code') 
+    classCode = models.CharField(max_length=200, verbose_name='Class Code')
     crn = models.IntegerField(default=0, verbose_name='CRN')
     schedule = models.CharField(max_length=200, verbose_name='Schedule')
     sortType = models.CharField(max_length=200, verbose_name='Sort Type')
@@ -13,10 +13,10 @@ class ClassWaitlist(models.Model):
     professor = models.ForeignKey(User, on_delete=models.CASCADE)
     date_added = models.DateTimeField("date published")
     closed = models.BooleanField(default=False)  # Add this line
-    anonymous_waitlist = models.BooleanField(default=False, verbose_name='Anonymous Waitlist') 
+    anonymous_waitlist = models.BooleanField(default=False, verbose_name='Anonymous Waitlist')
     request_academic_status = models.BooleanField(default=False, verbose_name='Request Academic Status')
     request_major = models.BooleanField(default=False, verbose_name='Request Major')
-    request_msg = models.BooleanField(default=False, verbose_name='Request Message')   
+    request_msg = models.BooleanField(default=False, verbose_name='Request Message')
 
     def __str__(self):
         return self.className
@@ -33,6 +33,9 @@ class StudentTicket(models.Model):
     student_major = models.CharField(max_length=100, verbose_name='Major', default='')
     msg = models.CharField(max_length=200, verbose_name='Message for Professor', default='')
     def __str__(self):
-        return str(self.student) + " - " + str(self.student.email) + ' : ' + str(self.date_joined.strftime("%d/%m/%Y, %H:%M:%S"))
-
-
+        return_msg = str(self.student) + " - " + str(self.student.email)
+        if self.student_major != '':
+            return_msg += " - " + str(self.student_major) + " Major"
+        if self.student_academic_status != '':
+            return_msg += " - " + str(self.student_academic_status)
+        return return_msg + ' : ' + str(self.date_joined.strftime("%d/%m/%Y, %H:%M:%S"))
