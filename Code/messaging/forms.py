3a598cfc71b9fb99caabed905fb9299a81f418e4
Isaac Isaac Mast <gimast@email.wm.edu>
2023-04-30 20:50:22 -0400

    Add messaging forms file and migrations

---
+++ Code/messaging/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Message
+
+
+class MessageForm(forms.ModelForm):
+    RECIPIENT_PLACEHOLDER = (None, "Select recipient...")
+
+    # Constructor for replied messages (no recipient choicefield)
+    def __init__(self, sender=None, recipient=None, users=None, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
+
+        if sender is None:
+            raise ValueError('There must be a sender to send a message.')
+        if recipient is None:
+            raise SyntaxError('To compose a new message, use the other constructor.')
+
+        self.fields['sender'] = sender.id
+
+        self.fields['body'].widget.attrs.update({ 'rows': 5 })
+
+    # Constructor for composed messages (recipient choicefield)
+    def __init__(self, sender=None, users=None, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
+
+        if sender is None:
+            raise ValueError('There must be a sender to send a message.')
+
+        self.fields['sender'] = sender.id
+
+        if len(users):
+            self.fields['receiver'].choices = users
+            self.fields['receiver'].choices.insert(0, self.RECIPIENT_PLACEHOLDER)
+        else:
+            self.fields['receiver'].choices = [self.RECIPIENT_PLACEHOLDER]
+
+        self.fields['body'].widget.attrs.update({ 'rows': 5 })
+        self.fields['receiver'].widget.attrs.update({
+            'id': 'compose-input-user',
+            'class': 'form-select'})
+
+    class Meta:
+        model = Message
+        fields = ['sender', 'receiver', 'body']
